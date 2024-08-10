b52b13625a6f87287cadd12b4993aced5ef97124
Ali Ganji <aliganji142879@gmail.com>
2024-08-10 16:45:13 +0330

    overriding create and model serializers

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth.models import User
 from rest_framework import status
 
 from .serializers import UserRegistrationSerializer
@@ -10,10 +9,6 @@ class UserRegister(APIView):
     def post(self, request):
         ser_data = UserRegistrationSerializer(data=request.POST)
         if ser_data.is_valid():
-            User.objects.create_user(
-                username=ser_data.validated_data['username'],
-                email=ser_data.validated_data['email'],
-                password=ser_data.validated_data['password'],
-            )
+            ser_data.create(ser_data.validated_data)
             return Response(ser_data.data, status=status.HTTP_201_CREATED)
-        return Response(ser_data.errors)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
